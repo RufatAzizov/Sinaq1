3ac1e2b1cb365ca2a4acf08c95cde5681bc31872
lemanzeynalova <mi7NJBDX9@code.edu.az>
2024-01-15 13:23:28 +0400

    Add project files.

---
+++ Sinaq1/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sinaq1.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Areas/Admin/Views/Home/Index.cshtml");
+        }
+    }
+}
---
+++ Sinaq1/Program.cs
@@ -0,0 +1,40 @@
+namespace Sinaq1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+                );
+            });
+
+            app.Run();
+        }
+    }
+}
